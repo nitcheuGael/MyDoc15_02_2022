c056642940cf4e73df24f416b99b3d590c04b0c9
Gael <gaelnitcheufatal@gmail.com>
2022-03-04 18:13:11 +0100

    messagerie de la landing page ok

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseApp } from '@angular/fire';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Constante } from '../entite/constante';
 import { AuthService } from '../service/auth.service';
+import { DialogConfirmService } from '../service/dialog-confirm.service';
 import { DialogConfirmeService } from '../service/Dialog/dialog-confirme.service';
+import { MailService } from '../service/smsMail/mail.service';
 
 @Component({
   selector: 'app-landing',
@@ -10,12 +14,16 @@ import { DialogConfirmeService } from '../service/Dialog/dialog-confirme.service
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  firstFormGroup!: FormGroup
+
   testbutonD = false
   testbutonC = true
   afficheCon = false
   button1 = true
   button2 = false
   title = 'app';
+  submitted = true;
+
 
 
   constructor(
@@ -23,17 +31,21 @@ export class LandingComponent implements OnInit {
     private auth: FirebaseApp,
     private dialogoService: DialogConfirmeService,
     private router: Router,
+    public serviceMailSms: MailService,
+    private message_confirme: DialogConfirmService
+
 
 
 
   ) { }
 
   ngOnInit(): void {
+    this.initialisationformulair()
+
     this.auth.auth().onAuthStateChanged((user) => {
       if (user) {
         this.testbutonD = true
         this.testbutonC = false
-
       }
     })
   }
@@ -69,6 +81,46 @@ export class LandingComponent implements OnInit {
     });
 
   }
+  get nom() {
+    return this.firstFormGroup!.get('nom')
+  }
+  get email() {
+    return this.firstFormGroup!.get('email')
+  }
+  get sujet() {
+    return this.firstFormGroup!.get('sujet')
+  }
+  get message() {
+    return this.firstFormGroup!.get('message')
+  }
+  /* Constante.emailNotification */
+  sendMail() {
+
+    var email = Constante.emailNotification + ',' + 'gaelnitcheufatal@gmail.com' + ',' + this.email?.value;
+    var email1 = '' + this.email?.value;
+    var objt = '' + this.sujet?.value;
+    var message = 'Nom :  ' + this.nom?.value + '    Email:   ' + email1 + '     Message:   ' + this.message?.value;
+    var entete = 'From: Mydoc@mydoc.cm'
+    this.serviceMailSms.sendEmail(email, objt, message, entete).then((projet) => {
+
+    }).catch(rejet => {
+      this.message_confirme.confirmActionAlertDialogue('Votre message sera pris en compte. Merci de nous avoir contacté.');
+      this.initialisationformulair()
+    })
+  }
+
+  initialisationformulair() {
+    //j'initialise le formulaire au demarage de l'application pour eviter certain burg
+    this.firstFormGroup = new FormGroup({
+      nom: new FormControl(''),
+      sujet: new FormControl(''),
+      email: new FormControl(''),
+      message: new FormControl(''),
+
+
+    })
+  }
+
 
 }
 
